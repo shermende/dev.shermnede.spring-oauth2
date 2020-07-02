0bba72945d59661b85328ee29e3e86320c8a39d5
abdys <n.u.abdysamat@gmail.com>
2020-07-02 11:57:31 +0600

    migrations

--- authorization-server/src/main/resources/db/migration/V1.0001__create-oauth.sql
+++ authorization-server/src/main/resources/db/migration/V1.0001__create-oauth.sql
@@ -45,10 +45,3 @@ CREATE TABLE oauth_code
     code           VARCHAR(255),
     authentication BLOB
 );
-
-insert into OAUTH_CLIENT_DETAILS (client_id, client_secret, scope, authorized_grant_types)
-values ('client', -- client-id
-        '$2a$10$PqaJjMII/AXm7jCTU4Srh.fq4G0YET8HvfsFVxgAtxQgWzek1Nma.', -- client-secret
-        'oauth2:any', -- scopes
-        'password,refresh_token' -- grant types
-       );
